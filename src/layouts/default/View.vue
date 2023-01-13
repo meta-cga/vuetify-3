75c75aaaee48e5a22ac1faf50ebdf86753ba3677
박영철 <notenore@frameout.com>
2023-01-13 23:32:49 +0900

    .

---
+++ src/layouts/default/View.vue
@@ -0,0 +1,11 @@
+// eslint-disable-next-line vue/multi-word-component-names
+// eslint-disable-next-line vue/multi-word-component-names
+<template>
+  <v-main>
+    <router-view />
+  </v-main>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script setup>
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<style lang="scss">
+  a {
+    span {
+      color: red;
+    }
+  }
+</style>
